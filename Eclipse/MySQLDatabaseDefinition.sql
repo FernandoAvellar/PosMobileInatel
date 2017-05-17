b02c71769f09d49ec20f42e8c29f809f7461e2d3
Fernando Avellar <fernando.avellar@inatel.br>
2017-05-17 10:24:20 -0300

    inserindo sql de definição do banco

---
+++ Eclipse/MySQLDatabaseDefinition.sql
@@ -0,0 +1,74 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           5.7.11-log - MySQL Community Server (GPL)
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando estrutura do banco de dados para projetofinaldm102
+CREATE DATABASE IF NOT EXISTS `projetofinaldm102` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `projetofinaldm102`;
+
+
+-- Copiando estrutura para tabela projetofinaldm102.atendimento
+CREATE TABLE IF NOT EXISTS `atendimento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  `data` datetime NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cliente_id` (`cliente_id`),
+  CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+
+
+-- Copiando estrutura para tabela projetofinaldm102.cliente
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `telefone` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+
+
+-- Copiando estrutura para tabela projetofinaldm102.cliente_fisico
+CREATE TABLE IF NOT EXISTS `cliente_fisico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(50) NOT NULL,
+  `identidade` varchar(20) NOT NULL,
+  `tipoDaIdentidade` varchar(20) NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_cliente_fisico_cliente` (`cliente_id`),
+  CONSTRAINT `FK_cliente_fisico_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+
+
+-- Copiando estrutura para tabela projetofinaldm102.cliente_juridico
+CREATE TABLE IF NOT EXISTS `cliente_juridico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(50) NOT NULL,
+  `razaoSocial` varchar(100) NOT NULL,
+  `inscricaoEstadual` varchar(50) NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_cliente_juridico_cliente` (`cliente_id`),
+  CONSTRAINT `FK_cliente_juridico_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
